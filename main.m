71aa8f862085a630e5725d201e3f74cdd20528fe
Koshy G <39202175+koshygeoji@users.noreply.github.com>
2020-07-20 14:57:31 +0530

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 % clc
 clear all
 close all
-im=imread('test images\8.jpg'); %test 3,4,5,8,9
+im=imread('test images\3.jpg'); %test 3,4,5,8,9
 figure,
 imshow(im), title('Original Image')
 im=double(im);
@@ -9,16 +9,9 @@ R=im(:,:,1);
 G=im(:,:,2);
 B=im(:,:,3);
 [row,col,dim]=size(im);
-for i=1:row
-    for j=1:col
-        first=R(i,j);
-        second=G(i,j);
-        third=B(i,j);
-        L(i,j)=max(first,max(second,third));
-    end
-end
-figure,
-imshow(L), title('Image Lightness')
+
+L=max(R,max(G,B));
+imshow(L,[]), title('Image Lightness')
 
 %illumination estimation
 r=7;
@@ -27,14 +20,14 @@ SE = strel('disk',r,n);
 Ilm = imclose(L,SE);
 Ilm=Ilm/255;
 figure,
-imshow(Ilm), title('Morphologically Closing Operation')
+imshow(Ilm,[]), title('Morphologically Closing Operation')
 
 guidedimg=rgb2hsv(im);
 g=guidedimg(:,:,3);
 I = imguidedfilter(Ilm,g);
 
 figure 
-imshow(I),title('Iluminance')
+imshow(I,[]),title('Iluminance')
 
 %reflectance
 rf1=R./I;
@@ -46,7 +39,7 @@ rel(:,:,1)=rf1;
 rel(:,:,2)=rf2;
 rel(:,:,3)=rf3;
 figure;
-imshow(rel,[]), title('Reflectance')
+imshow(uint8(rel),[]), title('Reflectance')
 
 %inputs
 I1=I;
@@ -65,6 +58,9 @@ alpha=2;
 phi=250;
 [imhue imsat imlum]=rgb2hsv(im);
 
+
+
+
 Wb1=exp(-((I1-0.5).^2)/(2*.25*.25));
 Wb2=exp(-((I2-0.5).^2)/(2*.25*.25));
 Wb3=exp(-((I3-0.5).^2)/(2*.25*.25));
@@ -77,6 +73,7 @@ W1=Wb1.*Wc1;
 W2=Wb2.*Wc2;
 W3=Wb3.*Wc3;
 
+%Normalize the weights
 W1=W1./(W1+W2+W3);
 W2=W2./(W1+W2+W3);
 W3=W3./(W1+W2+W3);
@@ -101,7 +98,7 @@ imshow(Ifinal1);title('Adjusted Illuminace ')
 
 %generate pyramid
 
-level=6;
+level=2;
 Lap_pyr_image1=genPyr(I1,'laplace',level);
 Gauss_pyr_weight1=genPyr(W1,'gauss',level);
 
@@ -131,20 +128,18 @@ end
 
 figure;
 imshow(Ifinal2), title('Adjusted Illuminance with pyramid')
-% Ifinal2=(2^level-1)*Ifinal2;
+
 enhanced1=rf1.*Ifinal2;
 enhanced2=rf2.*Ifinal2;
 enhanced3=rf3.*Ifinal2;
-
-% enhanced1=255*enhanced1/max(enhanced1(:));
-% enhanced2=255*enhanced2/max(enhanced2(:));
-% enhanced3=255*enhanced3/max(enhanced3(:));
 
 enhanced_color(:,:,1)=enhanced1;
 enhanced_color(:,:,2)=enhanced2;
 enhanced_color(:,:,3)=enhanced3;
 
-figure;
-imshow(uint8(enhanced_color),[]), title('Enhanced Image')
+enhanced_color_out=uint8(255*enhanced_color/max(enhanced_color(:)));
 
-piqe(uint8(enhanced_color))
+figure;
+imshow(enhanced_color_out), title('Enhanced Image')
+
+piqe(uint8(enhanced_color_out))
